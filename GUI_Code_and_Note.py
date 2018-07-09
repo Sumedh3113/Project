06d6e78e4bcbc65ab1f758c75839e2ff4a0d2513
sumedh3113 <sumedhdeshpande31@gmail.com>
2018-07-09 12:53:46 -0700

    update

--- GUI_Code_and_Note.py
+++ GUI_Code_and_Note.py
@@ -14,7 +14,6 @@ from tkinter import scrolledtext # for adding a scrollbar and text box
 Self-Notes: Here we arranged values in the form of 3 columns 
 so we want some widget in center we should change the column value from 0 to 1
 '''
-
 # Create instance
 win = tk.Tk()   
 
@@ -22,9 +21,10 @@ win = tk.Tk()
 win.title("Python GUI")
 
 '''
-We will create "Sumedh" a child of main window frame "win"
-and will replace every occurrence of win with Sumedh and all the widget will be contained in "Sumedh" 
+We will create "monty" a child of main window frame "win"
+and will replace every occurrence of win with monty and all the widget will be contained in "monty" 
 '''
+# We are creating a container frame to hold all other widgets
 monty = ttk.LabelFrame(tab1, text=' Monty Python ')
 monty.grid(column=0, row=0, padx=8, pady=4)
 
@@ -33,7 +33,7 @@ monty.grid(column=0, row=0, padx=8, pady=4)
 # we can hard code this value (1,2) etc
 
 # Modify adding a Label
-aLabel = ttk.Label(win, text="A Label")
+aLabel = ttk.Label(monty, text="A Label")
 aLabel.grid(column=0, row=0)
 
 #Modified Button Click Function
@@ -48,17 +48,17 @@ def clickMe():
 # Changing our Label
 # .grid() value changes as 00,01,10,11
 
-ttk.Label(win, text="Enter a name:").grid(column=0, row=0)
+ttk.Label(monty, text="Enter a name:").grid(column=0, row=0)
 
 # Adding a Textbox Entry widget
 name = tk.StringVar() # to take input from user
-nameEntered = ttk.Entry(win, width=12, textvariable=name) 
+nameEntered = ttk.Entry(monty, width=12, textvariable=name) 
 # testvariable is assigned to input variable
 nameEntered.grid(column=0, row=1)
 
 
 # Adding a Button 
-action = ttk.Button(win, text="Click Me!", command=clickMe)   
+action = ttk.Button(monty, text="Click Me!", command=clickMe)   
 # command  for calling clickMe fumction
 action.grid(column=2, row=1)
 #action.configure(state='disabled')   
@@ -68,9 +68,9 @@ action.grid(column=2, row=1)
 Adding a combobox to select from drop down
 state is readonly so that user will not able to write in combo box
 '''
-ttk.Label(win, text="Select a Number:").grid(column=1, row=0)
+ttk.Label(monty, text="Select a Number:").grid(column=1, row=0)
 number = tk.StringVar()
-numberchose = ttk.Combobox(win, width = 12, textvariable=number, state = 'readonly')
+numberchose = ttk.Combobox(monty, width = 12, textvariable=number, state = 'readonly')
 numberchose['values'] = (1,2,3,4,100)
 numberchose.grid(column =1, row = 1)
 numberchose.current(0)
@@ -81,7 +81,7 @@ numberchose.current(0)
 
 
 chVarDis = tk.IntVar()# this variable will be created for every check button as we can select multiple buttons
-check1 = tk.Checkbutton(win, text="Disabled", variable=chVarDis, state='disabled')
+check1 = tk.Checkbutton(monty, text="Disabled", variable=chVarDis, state='disabled')
 check1.select() 
 # to select the checkbox
 check1.grid(column=0, row=4, sticky=tk.W) 
@@ -89,14 +89,14 @@ check1.grid(column=0, row=4, sticky=tk.W)
              
 
 chVarUn = tk.IntVar()
-check2 = tk.Checkbutton(win, text="UnChecked", variable=chVarUn) 
+check2 = tk.Checkbutton(monty, text="UnChecked", variable=chVarUn) 
 # variable is assigned to intvar()
 check2.deselect() 
 # to deselect the checkbox
 check2.grid(column=1, row=4, sticky=tk.W)                   
 
 chVarEn = tk.IntVar()
-check3 = tk.Checkbutton(win, text="Enabled", variable=chVarEn)
+check3 = tk.Checkbutton(monty, text="Enabled", variable=chVarEn)
 check3.select()
 check3.grid(column=2, row=4, sticky=tk.W)                     
 '''
@@ -120,13 +120,13 @@ def radCall():
 # create three Radiobuttons using one variable
 radVar = tk.IntVar()
 
-rad1 = tk.Radiobutton(win, text=COLOR1, variable=radVar, value=1, command=radCall)# from Command we called radCall method and value of radVar will be passed 
+rad1 = tk.Radiobutton(monty, text=COLOR1, variable=radVar, value=1, command=radCall)# from Command we called radCall method and value of radVar will be passed 
 rad1.grid(column=0, row=5, sticky=tk.W, columnspan=3)   
 
-rad2 = tk.Radiobutton(win, text=COLOR2, variable=radVar, value=2, command=radCall)
+rad2 = tk.Radiobutton(monty, text=COLOR2, variable=radVar, value=2, command=radCall)
 rad2.grid(column=1, row=5, sticky=tk.W, columnspan=3)  
 
-rad3 = tk.Radiobutton(win, text=COLOR3, variable=radVar, value=3, command=radCall)
+rad3 = tk.Radiobutton(monty, text=COLOR3, variable=radVar, value=3, command=radCall)
 rad3.grid(column=2, row=5, sticky=tk.W, columnspan=3)
 
 '''
@@ -134,7 +134,7 @@ rad3.grid(column=2, row=5, sticky=tk.W, columnspan=3)
 # Using a scrolled Text control    
 scrolW  = 30  # These are the std values which we use but we can change them for experimental purpose
 scrolH  =  3
-scr = scrolledtext.ScrolledText(win, width=scrolW, height=scrolH, wrap=tk.WORD)# wrap=tk.WORD to break lines by words 
+scr = scrolledtext.ScrolledText(monty, width=scrolW, height=scrolH, wrap=tk.WORD)# wrap=tk.WORD to break lines by words 
 																			   #if we use wrap = tk.char then will wrap around the word
 #scr.grid(column=0, sticky='WE', columnspan=3)# sticky = WE is for west alignment 
 scr.grid(column=0, columnspan=3)# so our scroll text will have 3 column of space initially
@@ -165,13 +165,13 @@ for col in range(3):
 	'''# if we want to create 100 of these radio buttons we just have to change value from 3 to 100
     curRad = 'rad' + str(col) # what does it do?  It is just taking string rad and converting value of column to str() '''
 	using type casting and then just storing it so 3 line above will reduced to 1 line 
-    curRad = tk.Radiobutton(win, text=colors[col], variable=radVar, value=col, command=radCall)
+    curRad = tk.Radiobutton(monty, text=colors[col], variable=radVar, value=col, command=radCall)
     curRad.grid(column=col, row=6, sticky=tk.W)
 '''
 '''
 # Create a container to hold labels
 
-labelsFrame = ttk.LabelFrame(win, text=' Labels in a Frame ')
+labelsFrame = ttk.LabelFrame(monty, text=' Labels in a Frame ')
 labelsFrame.grid(column=0, row=7, padx=20, pady=40)
 
 
